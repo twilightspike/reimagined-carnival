8ee59322031fdf2a5f9c953c9e44f1516e034fe9
twilightspike <twilightspike@users.noreply.github.com>
2017-06-27 15:47:59 +0800

    Update 2017practice000.boot.js

--- main/js/2017practice000.boot.js
+++ main/js/2017practice000.boot.js
@@ -1,2 +1,10 @@
 var Carnival = {};
-Carnival.Boot = function(){};
+Carnival.Boot = function(game){};
+Carnival.Boot.prototype = {
+  preload: function(){
+    this.load.image('', '');
+  },
+  create: function(){
+    this.create.image('', '');
+  }
+};
